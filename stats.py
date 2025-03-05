6d797c33c35a3090653f6cef047716418b46652f
Paul G. Arias <paulgarias@gmail.com>
2025-03-05 13:03:39 -0500

    Added standard deviation and median functions.

--- stats.py
+++ stats.py
@@ -8,3 +8,17 @@ def calculate_variance(data):
         return 0
     mean = calculate_mean(data)
     return sum((x - mean) ** 2 for x in data) / len(data)
+
+def calculate_standard_deviation(data):
+    import math
+    return math.sqrt(calculate_variance(data))
+
+def calculate_median(data):
+    if not data:
+        return 0
+    data.sort()
+    n = len(data)
+    if n % 2 == 0:
+        return (data[n // 2 - 1] + data[n // 2]) / 2
+    else:
+        return data[n // 2]
